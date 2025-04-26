a8f10bf01c2886be9e8d44eeb26ee2f6656d6cd2
Renessh <simalyka02@gmail.com>
2025-04-26 16:44:31 +0300

    Update 1.py
    with love for you

--- 1.py
+++ 1.py
@@ -1,1 +1,11 @@
+from random import randint
+from time import sleep
 
+a = int(input('Введит число от:'))
+b = int(input('Введит число до:'))
+
+result = randint(a, b)
+
+print(f'подбираем число в диапзоне от {a} до {b}...')
+sleep(2)
+print(f'Случайное число: {result}')
